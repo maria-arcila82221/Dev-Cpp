aac644fa4e2fe760d2de2b41bcc95225a1c0932a
María José Arcila Cano <maria_arcila82221@elpoli.edu.co>
2023-04-28 08:48:57 -0500

    Initial commit

---
+++ Introduccin C++.cpp
@@ -0,0 +1,27 @@
+/*
+	Name: 
+	Copyright: 
+	Author: María José Arcila Cano
+	Date: 15/06/22 12:07
+	Description: 
+*/
+
+using namespace std;
+
+# include <iostream>
+# include <stdlib.h>
+# include <string.h>
+
+int main ()
+{
+	int Edad;
+	float Peso;
+	char Sex;
+	string Nom;
+	
+	cout << "Ingrese su nombre: ";
+	cin >> Nom;
+	cout << "HOLA MUNDO SOY: " << Nom << "\n\n";
+	
+	return EXIT_SUCCESS;
+}
